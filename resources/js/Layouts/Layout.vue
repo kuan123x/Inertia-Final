5b18397a432c9f4e14da3fe8ba0a9f52a7151527
kuan123x <mathewcarillo143@gmail.com>
2024-05-26 20:07:57 +0800

    first commit

---
+++ resources/js/Layouts/Layout.vue
@@ -0,0 +1,173 @@
+<template>
+    <div id="main-wrapper">
+        <div id="sidebar" class="min-w-[500px] bg-gradient-to-b from-gray-700 to-gray-500 flex flex-col items-center">
+            <div id="branding">
+                <img src="https://media1.tenor.com/m/xlFZTbLqs20AAAAd/itachi.gif" alt="">
+            </div>
+
+            <div>
+                <h1 class="mt-[10%] fw-bold">Mathew Jerome Carillo<br> <em>(kuan)</em></h1>
+                <h4>BSIT-3</h4>
+            </div>
+
+            <div class="mt-[20%]">
+                <Link href="/projects" class="button-53"> Projects</Link>
+                <Link href="/landing" class="button-about-me">About Me</Link>
+            </div>
+        </div>
+        <div id="content" class="flex flex-col flex-1">
+            <div class="flex-1 p-0">
+                <Transition name="page-trans" mode="out-in">
+                    <div>
+                        <slot />
+                    </div>
+                </Transition>
+            </div>
+            <!-- <footer class="py-4 text-gray-600 text-center bg-white" style="font-style: italic;">
+                Inertia Final Project 2024
+            </footer> -->
+        </div>
+    </div>
+</template>
+
+<script setup>
+    import { Link } from "@inertiajs/vue3";
+</script>
+
+<style scoped>
+#sidebar {
+    min-width: 350px;
+    background-color: #d5eae6;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+.button-53 {
+    background-color: #3d52b1;
+    border: 0 solid #E5E7EB;
+    box-sizing: border-box;
+    color: #000000;
+    display: flex;
+    font-family: ui-sans-serif,system-ui,-apple-system,system-ui,"Segoe UI",Roboto,"Helvetica Neue",Arial,"Noto Sans",sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol","Noto Color Emoji";
+    font-size: 1rem;
+    margin-bottom: 20px;
+    font-weight: 700;
+    justify-content: center;
+    line-height: 1.75rem;
+    padding: .75rem 1.65rem;
+    position: relative;
+    text-align: center;
+    text-decoration: none #000000 solid;
+    text-decoration-thickness: auto;
+    width: 100%;
+    max-width: 460px;
+    position: relative;
+    cursor: pointer;
+    transform: rotate(-2deg);
+    user-select: none;
+    -webkit-user-select: none;
+    touch-action: manipulation;
+    border-radius: 5px;
+    transition: all 0.3s ease;
+}
+
+.button-53:focus {
+    outline: 0;
+}
+
+.button-53:after {
+    content: '';
+    position: absolute;
+    border: 1px solid #000000;
+    bottom: 4px;
+    left: 4px;
+    width: calc(100% - 1px);
+    height: calc(100% - 1px);
+}
+
+.button-53:hover:after {
+    bottom: 2px;
+    left: 2px;
+}
+
+@media (min-width: 768px) {
+    .button-53 {
+        padding: .75rem 3rem;
+        font-size: 1.25rem;
+    }
+}
+
+.button-about-me {
+    background-color: #0bdaad;
+    border: 1 solid #E5E7EB;
+    box-sizing: border-box;
+    color: #000000;
+    display: flex;
+    font-family: ui-sans-serif,system-ui,-apple-system,system-ui,"Segoe UI",Roboto,"Helvetica Neue",Arial,"Noto Sans",sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol","Noto Color Emoji";
+    font-size: 1rem;
+    font-weight: 700;
+    justify-content: center;
+    line-height: 1.75rem;
+    padding: .75rem 1.65rem;
+    position: relative;
+    text-align: center;
+    text-decoration: none #000000 solid;
+    text-decoration-thickness: auto;
+    width: 100%;
+    max-width: 460px;
+    position: relative;
+    cursor: pointer;
+    transform: rotate(-2deg);
+    user-select: none;
+    -webkit-user-select: none;
+    touch-action: manipulation;
+    border-radius: 5px;
+    transition: all 0.3s ease;
+}
+
+.button-about-me:focus {
+    outline: 0;
+}
+
+.button-about-me:after {
+    content: '';
+    position: absolute;
+    border: 1px solid #000000;
+    bottom: 4px;
+    left: 4px;
+    width: calc(100% - 1px);
+    height: calc(100% - 1px);
+}
+
+.button-about-me:hover:after {
+    bottom: 2px;
+    left: 2px;
+}
+
+h4 {
+    margin-top: 5%;
+    font-size: 30px;
+    color: black;
+    /* font-size: 25px; */
+}
+
+h1 {
+    font-size: 30px;
+    color: black;
+}
+
+#main-wrapper {
+    min-height: 100vh;
+    display: flex;
+    background-color: #5f6b79;
+    min-height: 100vh;
+    width: 100%;
+}
+
+#branding img {
+    width: 200px;
+    height: 200px;
+    border-radius: 150px;
+    margin-top: 24px;
+}
+</style>
---
+++ resources/js/Pages/Landing.vue
@@ -0,0 +1,123 @@
+<script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import Layout from '@/Layouts/Layout.vue';
+import { Link } from '@inertiajs/vue3';
+import '@fortawesome/fontawesome-free/css/all.css';
+import '@fortawesome/fontawesome-free/js/all.js';
+
+defineOptions({
+    layout: Layout
+});
+</script>
+
+<template>
+  <AuthenticatedLayout>
+    <div class="landing-container fade-in">
+      <Sidebar></Sidebar>
+      <div class="landing-content slide-up">
+        <div class="about-me-card">
+          <div class="about-me">
+            <p class="text-lg leading-relaxed text-start">
+              Hey, I'm Mathew Jerome Carillo, a third-year BSIT student at Mater Dei College. Alongside my studies, I enjoy honing my coding skills and exploring new technologies. In my free time, I indulge in playing online games, balancing my academic pursuits with some recreational fun. Thanks for visiting my page!
+            </p>
+            <div class="social-media-buttons">
+          <a href="https://www.instagram.com" class="social-media-button">
+            <i class="fab fa-instagram"></i>
+          </a>
+          <a href="https://twitter.com" class="social-media-button">
+            <i class="fab fa-twitter"></i>
+          </a>
+          <a href="https://www.facebook.com" class="social-media-button">
+            <i class="fab fa-facebook-f"></i>
+          </a>
+        </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </AuthenticatedLayout>
+</template>
+
+<style scoped>
+.landing-container {
+  display: flex;
+  height: 100vh;
+  width: 100%;
+  padding: 100px;
+}
+
+.landing-content {
+  flex: 1;
+  padding-left: 70px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+.about-me-card {
+  background: rgba(255, 255, 255, 0.3); /* Semi-transparent background */
+  backdrop-filter: blur(10px); /* Blur effect */
+  padding: 20px;
+  border-radius: 10px;
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+  margin-bottom: 20px;
+}
+
+.about-me {
+  margin-bottom: 0rem;
+}
+
+.about-me h2 {
+  font-size: 3rem;
+}
+
+p {
+  font-size: 35px;
+}
+
+.social-media-buttons {
+  display: flex;           /* Flexbox layout */
+  justify-content: center; /* Center the icons horizontally */
+  margin-top: 40px;
+}
+
+.social-media-button {
+  font-size: 40px; /* Increased font size */
+  margin-right: 40px; /* Increased margin for better spacing */
+  color: #000;
+  text-decoration: none;
+  transition: color 0.3s ease;
+}
+
+.social-media-button:hover {
+  color: #2e323a;
+}
+
+@keyframes fadeIn {
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+}
+
+.fade-in {
+  animation: fadeIn 1s ease-in-out;
+}
+
+@keyframes slideUp {
+  from {
+    transform: translateY(100%);
+  }
+  to {
+    transform: translateY(0);
+  }
+}
+
+.slide-up {
+  animation: slideUp 1s ease-out;
+}
+</style>
